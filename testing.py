9b76734d617aa3b10fb61029388e7b8a9f83f7aa
mrcai <mrcai@asu.edu>
2020-05-15 23:16:06 -0700

    More Pseudocode

--- testing.py
+++ testing.py
@@ -12,13 +12,21 @@ https://github.com/TwitchIO/TwitchIO/blob/master/twitchio/dataclasses.py
 """
 class TestBotMethods(unittest.TestCase):
     
+    # Need to make the test bot utilize a temporary settings file
     def commandCalledTest(self):
+        """
+        Test that the correct method is called upon the correct chat message
+        """
         mockctx = unittest.mock.MagicMock(twitchio.Message)
         mockctx.content = unittest.mock.Mock(return_value='@Haurbus')
         bot.event_message(mockctx)
         #Modules.basics.get_bot_info()
+        self.assertEqual(True, True)
 
     def correctValueTest(self):
+        """
+        Test that the correct output is created for the chat message content
+        """
         mockctx = unittest.mock.MagicMock(twitchio.Message)
         # Add mock ctx content (mod here)
         Modules.basics.get_shoutout(mockctx)
